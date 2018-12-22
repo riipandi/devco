04b3792ce69e99186cde8602fc0aceddac77141a
Tim Ruhay <timruhay@gmail.com>
2018-12-22 22:47:30 +0700

    Initial API setup

---
+++ sample.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# run:
+# gunicorn -b 127.0.0.1:2368 sample:api
+
+from falcon import falcon
+
+class QuoteResource:
+    def on_get(self, req, resp):
+        """Handles GET requests"""
+        quote = {
+            'quote': (
+                "I've always been more interested in "
+                "the future than in the past."
+            ),
+            'author': 'Grace Hopper'
+        }
+
+        resp.media = quote
+
+api = falcon.API()
+api.add_route('/', QuoteResource())
